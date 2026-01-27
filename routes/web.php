d2dbf044103d329577bb89e3998874fe1bae6df9
renzolinsangan <renzolinsangan11@gmail.com>
2026-01-27 20:51:26 +0800

    Global Constraints and workaround for the acquiring/passing of data in routes.

--- routes/web.php
+++ routes/web.php
@@ -7,41 +7,70 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// FOR SIMPLE GET ROUTING
 Route::get('/jobs', function () {
     return '<h1>Available Jobs</h1>';
+})->name('jobs');
+
+// SIMPLE PASSING OF PARAMETER THROUGH ANY PARAMETERS
+Route::get('/posts/{id}', function (string $id) {
+    return 'Post ' . $id;
 });
+
+// PASSING OF PARAMETER THAT ACCEPTS ONLY INTEGER
+Route::get('/intonly/{id}', function (string $id) {
+    return 'Post ' . $id;
+}); // with global routing, can be found in AppServiceProvider
+// })->whereNumber('id'); // shortcut
+// })->where('id', '[0-9]+'); // for longer syntax
+
+// PASSING OF PARAMETER THAT ACCEPTS ONLY STRING
+Route::get('/stringonly/{name}', function (string $name) {
+    return 'Post ' . $name;
+}); // with global routing, can be found in AppServiceProvider
+// })->whereAlpha('id'); // shortcut
+// })->where('id', '[a-zA-z]+'); // for longer syntax
+
+Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return 'Post ' . $id . ' Comments ' . $commentId;
+});
+
+// =============================== SYNTAX 1 ========================================= //
+// FOR SIMPLE GET ROUTING
+// Route::get('/jobs', function () {
+//     return '<h1>Available Jobs</h1>';
+// });
 
 // FOR PROVIDING NAME FROM THE ROUTE
-Route::get('/getjobs', function () {
-    return '<h1>Available Get Jobs</h1>';
-})->name('getjobs');
+// Route::get('/getjobs', function () {
+//     return '<h1>Available Get Jobs</h1>';
+// })->name('getjobs');
 
 // FOR POST SUBMIT ROUTING
-Route::post('/submit', function () {
-    return 'Submitted';
-});
+// Route::post('/submit', function () {
+//     return 'Submitted';
+// });
 
 // FOR MATCHING BASED ON THE ARRAY ARGUMENT BEFORE THE ROUTING PAGE NAME
-Route::match(['get', 'post'], '/confirm', function () {
-    return 'Confirmed';
-});
+// Route::match(['get', 'post'], '/confirm', function () {
+//     return 'Confirmed';
+// });
 
 // ANY TYPE WILL BE CONSIDER, GET, POST, DELETE ETC.
-Route::any('/anysubmit', function () {
-    return 'Anything';
-});
+// Route::any('/anysubmit', function () {
+//     return 'Anything';
+// });
 
 // ROUTING BASED FROM THE NAME OF THE ROUTE FOR CONNECTING ONE ROUTE TO ANOTHER ROUTE
-Route::get('/test', function () {
-    $url = route('getjobs');
-    return "<a href='$url'>Click here!</a>";
-});
+// Route::get('/test', function () {
+//     $url = route('getjobs');
+//     return "<a href='$url'>Click here!</a>";
+// });
 
 // ROUTING FOR PASSING JSON DATA
-Route::get('/api/users', function () {
-    return [
-        'name' => 'John Doe',
-        'email' => 'johndoe@gmail.com'
-    ];
-});
+// Route::get('/api/users', function () {
+//     return [
+//         'name' => 'John Doe',
+//         'email' => 'johndoe@gmail.com'
+//     ];
+// });
+// =============================== SYNTAX 1 ========================================= //
