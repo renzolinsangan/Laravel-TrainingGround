f3f1401498bbfdca2f0d1639d8072b4a541aecb0
renzolinsangan <renzolinsangan11@gmail.com>
2026-01-27 20:33:05 +0800

    Lesson from MVC and Routing structure in laravel.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,46 @@
 
 use Illuminate\Support\Facades\Route;
 
+// FOR THE MAIN PAGE
 Route::get('/', function () {
     return view('welcome');
+});
+
+// FOR SIMPLE GET ROUTING
+Route::get('/jobs', function () {
+    return '<h1>Available Jobs</h1>';
+});
+
+// FOR PROVIDING NAME FROM THE ROUTE
+Route::get('/getjobs', function () {
+    return '<h1>Available Get Jobs</h1>';
+})->name('getjobs');
+
+// FOR POST SUBMIT ROUTING
+Route::post('/submit', function () {
+    return 'Submitted';
+});
+
+// FOR MATCHING BASED ON THE ARRAY ARGUMENT BEFORE THE ROUTING PAGE NAME
+Route::match(['get', 'post'], '/confirm', function () {
+    return 'Confirmed';
+});
+
+// ANY TYPE WILL BE CONSIDER, GET, POST, DELETE ETC.
+Route::any('/anysubmit', function () {
+    return 'Anything';
+});
+
+// ROUTING BASED FROM THE NAME OF THE ROUTE FOR CONNECTING ONE ROUTE TO ANOTHER ROUTE
+Route::get('/test', function () {
+    $url = route('getjobs');
+    return "<a href='$url'>Click here!</a>";
+});
+
+// ROUTING FOR PASSING JSON DATA
+Route::get('/api/users', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'johndoe@gmail.com'
+    ];
 });
