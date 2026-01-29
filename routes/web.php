5a64238df6d21725e2b40b442306df9920c044eb
renzolinsangan <renzolinsangan11@gmail.com>
2026-01-29 19:54:31 +0800

    Request methods and query params

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 // FOR THE MAIN PAGE
 Route::get('/', function () {
@@ -11,28 +12,7 @@ Route::get('/jobs', function () {
     return '<h1>Available Jobs</h1>';
 })->name('jobs');
 
-// SIMPLE PASSING OF PARAMETER THROUGH ANY PARAMETERS
-Route::get('/posts/{id}', function (string $id) {
-    return 'Post ' . $id;
-});
 
-// PASSING OF PARAMETER THAT ACCEPTS ONLY INTEGER
-Route::get('/intonly/{id}', function (string $id) {
-    return 'Post ' . $id;
-}); // with global routing, can be found in AppServiceProvider
-// })->whereNumber('id'); // shortcut
-// })->where('id', '[0-9]+'); // for longer syntax
-
-// PASSING OF PARAMETER THAT ACCEPTS ONLY STRING
-Route::get('/stringonly/{name}', function (string $name) {
-    return 'Post ' . $name;
-}); // with global routing, can be found in AppServiceProvider
-// })->whereAlpha('id'); // shortcut
-// })->where('id', '[a-zA-z]+'); // for longer syntax
-
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post ' . $id . ' Comments ' . $commentId;
-});
 
 // =============================== SYNTAX 1 ========================================= //
 // FOR SIMPLE GET ROUTING
@@ -74,3 +54,73 @@ Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $com
 //     ];
 // });
 // =============================== SYNTAX 1 ========================================= //
+
+// =============================== SYNTAX 2 ========================================= //
+// SIMPLE PASSING OF PARAMETER THROUGH ANY PARAMETERS
+// Route::get('/posts/{id}', function (string $id) {
+//     return 'Post ' . $id;
+// });
+
+// PASSING OF PARAMETER THAT ACCEPTS ONLY INTEGER
+// Route::get('/intonly/{id}', function (string $id) {
+//     return 'Post ' . $id;
+// }); // with global routing, can be found in AppServiceProvider
+// })->whereNumber('id'); // shortcut
+// })->where('id', '[0-9]+'); // for longer syntax
+
+// PASSING OF PARAMETER THAT ACCEPTS ONLY STRING
+// Route::get('/stringonly/{name}', function (string $name) {
+//     return 'Post ' . $name;
+// }); // with global routing, can be found in AppServiceProvider
+// })->whereAlpha('id'); // shortcut
+// })->where('id', '[a-zA-z]+'); // for longer syntax
+
+// Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+//     return 'Post ' . $id . ' Comments ' . $commentId;
+// });
+// =============================== SYNTAX 2 ========================================= //
+
+// =============================== SYNTAX 3 ========================================= //
+// FOR PROCESSING THE REQUEST USE
+// Route::get('/test', function (Request $request) {
+//     return [
+//         'method' => $request->method(),
+//         'url' => $request->url(),
+//         'path' => $request->path(),
+//         'fullUrl' => $request->fullUrl(),
+//         'ip' => $request->ip(),
+//         'userAgent' => $request->userAgent(),
+//         'header' => $request->header()
+//     ];
+// });
+
+// FOR QUERY PARAMS ACQUIRING DATA
+// Route::get('/users', function (Request $request) {
+//     return $request->query('name');
+// });
+
+// FOR QUERY PARAMS ACQUIRING MULTIPLE DATA
+// Route::get('/users', function (Request $request) {
+//     return $request->only(['name', 'age']);
+// });
+
+// FOR QUERY PARAMS ACQURING ALL DATA
+// Route::get('/users', function (Request $request) {
+//     return $request->all();
+// });
+
+// FOR QUERY PARAMS CHECKING IF THERE IS DATA OR NOT, IT IS IN BOOLEAN FORM
+// Route::get('/users', function (Request $request) {
+//     return $request->has('name');
+// });
+
+// FOR QUERY PARAMS THAT WILL WORK BOTH IN URL AND INPUT FORM FIELDS
+// Route::get('/users', function (Request $request) {
+//     return $request->input('name', 'Default Name'); // second parameter is for setting default value
+// });
+
+// FOR QUERY PARAMS THAT WILL PROVIDE AN EXCEPTION TO NOT BE INCLUDED IN THE RETURN REQUEST
+// Route::get('/users', function (Request $request) {
+//     return $request->except(['name']);
+// });
+// =============================== SYNTAX 3 ========================================= //
