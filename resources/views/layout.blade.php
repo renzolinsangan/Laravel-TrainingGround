76aea0fac4c829cc37ebf77cbecfe318f5bc8f1c
renzolinsangan <renzolinsangan11@gmail.com>
2026-02-02 21:08:54 +0800

    Added Hero in the webpage

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    {{-- asset goes directly to the public folder --}}
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>{{$title ?? 'Workopia | Find and list jobs'}}</title>
 </head>
 <body class="bg-gray-100">
     <x-header />
+    @if(request()->is('/'))
+    <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
