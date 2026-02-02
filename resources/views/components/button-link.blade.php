0971755c1eda48c3c01a87652274f16f246fb259
renzolinsangan <renzolinsangan11@gmail.com>
2026-02-02 20:27:31 +0800

    ButtonLink Component

---
+++ resources/views/components/button-link.blade.php
@@ -0,0 +1,15 @@
+{{-- IF IT WAS SET TO DEFAULT, NO NEED TO CALL IT IN THE COMPONENT --}}
+@props([
+    'url' => '/', 
+    'icon' => null, 
+    'bgClass' => 'bg-yellow-500', 
+    'hoverClass' => 'hover:bg-yellow-600', 
+    'textClass' => 'text-black'
+])
+
+<a href="{{$url}}" class="{{$bgClass}} {{$hoverClass}} {{$textClass}} px-4 py-2 rounded hover:shadow-md transition duration-300">
+    @if($icon)
+        <i class="fa fa-{{$icon}} mr-1"></i>
+    @endif
+    {{$slot}} {{-- $slot for the attributes or text inside the component tag --}}
+</a>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,9 +11,7 @@
             <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
             <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
             <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
-            <a href="{{url('/jobs/create')}}" class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
-                <i class="fa fa-edit"></i> Create Job
-            </a>
+            <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link> {{-- IF IT HAS DEFAULT VALUE, NO NEED TO CALL IT IN THE COMPONENT --}}
         </nav>
         <button
             id="hamburger"
